0bf8210f16dfcbb7385a5294129316e2e7b5c3b1
Akmaral <21121971@mail.ru>
2022-11-20 18:30:45 +0600

    first commit

---
+++ task2.py
@@ -0,0 +1,6 @@
+a = int(input())
+print(a // 10 % 10)
+
+#a = input()
+#b =  a[1] if len(a) != 1 else a[0]
+#print(b)
---
+++ task9.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+if a == b and a == c and c == b:
+    print(3)
+elif a == b or a == c or b == c:
+    print(2)
+else:
+    print(0)
+    
+
+
+
